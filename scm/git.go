02d9011fdc1c75ad71e2ab5ae32adc3236c790e4
Maximilian Pachl <m@ximilian.info>
2017-08-11 23:06:41 +0200

    skeleton for git branch poller

--- scm/git.go
+++ scm/git.go
@@ -15,3 +15,78 @@ package scm
 // You should have received a copy of the GNU General Public License
 // along with this program.  If not, see <http://www.gnu.org/licenses/>.
 
+// --------------------------------------------------------------------------------------
+//  imports
+// --------------------------------------------------------------------------------------
+
+import (
+    "strings"
+    "time"
+
+    "gopkg.in/src-d/go-git.v4/plumbing/transport/client"
+    "gopkg.in/src-d/go-git.v4/plumbing/transport"
+
+    "github.com/faryon93/sackci/log"
+)
+
+
+// --------------------------------------------------------------------------------------
+//  public functions
+// --------------------------------------------------------------------------------------
+
+func GitWatch(url string, branch string) {
+    session, err := git(url)
+    if err != nil {
+        log.Error("git", "failed to connect to git repo:", err.Error())
+        return
+    }
+
+    // cyclically check for a new revision in the branch we are watching
+    for {
+        // get all the references
+        ar, err := session.AdvertisedReferences()
+        if err != nil {
+            log.Error("git", "failed to get references:", err.Error())
+            continue
+        }
+
+        refs, err := ar.AllReferences()
+        if err != nil {
+            log.Error("git", "failed to parse references:", err.Error())
+            return
+        }
+
+        // find the reference of the correct branch
+        found := false
+        for _, ref := range refs {
+            name := strings.Split(ref.Name().String(), "/")
+
+            if ref.IsBranch() && name[2] == branch {
+                log.Info("git", "branch reference:", ref.Hash().String())
+                found = true
+                break
+            }
+        }
+
+        if !found {
+            log.Error("git", "repository has no branch \"" + branch + "\"")
+            return
+        }
+
+        time.Sleep(1 * time.Second)
+    }
+}
+
+func git(url string) (transport.UploadPackSession, error) {
+    endpoint, err := transport.NewEndpoint(url)
+    if err != nil {
+        return nil, err
+    }
+
+    clnt, err := client.NewClient(endpoint)
+    if err != nil {
+        return nil, err
+    }
+
+   return clnt.NewUploadPackSession(endpoint, nil)
+ }
